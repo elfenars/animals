6864646dbbfe338a36af665280e49fe778bda3ad
Daniel Lopez Rivas <lopezrivasdaniel@gmail.com>
2014-12-27 19:02:21 -0300

    Devise routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -83,6 +83,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :tipo, :description, :image)
+      params.require(:post).permit(:title, :tipo, :description, :image, :user_id)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+
+	belongs_to :user
 	attr_accessor :image
 
 	mount_uploader :image, ImageUploader
