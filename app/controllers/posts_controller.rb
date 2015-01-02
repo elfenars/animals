8bad319fed1d489ce717ea6bdee19f35e07ec9f0
Daniel Lopez Rivas <lopezrivasdaniel@gmail.com>
2015-01-02 18:37:12 -0300

    Arreglo de diseño en forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -77,7 +77,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :tipo, :description, :image, :status, :contact, :user_id)
+      params.require(:post).permit(:title, :tipo, :description, :image, :location, :status, :contact, :user_id)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231204328) do
+ActiveRecord::Schema.define(version: 20150102191445) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141231204328) do
     t.integer  "user_id"
     t.string   "status"
     t.string   "contact"
+    t.string   "location"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
