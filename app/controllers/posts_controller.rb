0a5805367b94d8734ebfa12805d9465706d7dae9
Feña Agar <ferliagno@gmail.com>
2015-01-08 16:53:15 -0300

    removed scaffold comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,20 @@
 class PostsController < ApplicationController
-
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
   def index
-    
     @encontrados = Post.where(tipo: "encontrado").order(created_at: :desc).limit(3)
     @perdidos = Post.where(tipo: "perdido").order(created_at: :desc).limit(3)
     @adopcion = Post.where(tipo: "adopcion").order(created_at: :desc).limit(3)
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     authenticate_user!
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
     authenticate_user!
   end
@@ -39,8 +31,6 @@ class PostsController < ApplicationController
     @adopcion = Post.where(tipo: 'adopcion').page(params[:page]).order(created_at: :desc)
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = current_user.posts.new(post_params)
       if @post.save
@@ -62,8 +52,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -73,12 +61,11 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :tipo, :description, :image, :location, :status, :contact, :animal_type, :age, :breed, :user_id)
     end
