8fb9bd89080f9c92a94438e5404c60cea3b996da
Daniel Lopez Rivas <lopezrivasdaniel@gmail.com>
2015-01-13 18:37:44 -0300

    Admin controller and views

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -2,5 +2,8 @@ class Dashboard::BaseController < ApplicationController
 	layout "dashboard"
 
 	before_action :require_admin
+	def require_admin
+		redirect_to login_path unless current_user.admin
+	end
 
 end
